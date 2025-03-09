92fcd5b3c6386baaa58c6d9e97b1126d73b55831
Mariana Gonzalez Medina <mgm_14392@hotmail.com>
2025-03-09 16:55:26 +0100

    update dockerfile

--- ui.py
+++ ui.py
@@ -28,4 +28,5 @@ def index():
     ''')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+    port = int(os.environ.get("PORT", 8080))  # Cloud Run sets PORT as an environment variable
+    app.run(host='0.0.0.0', port=port)
